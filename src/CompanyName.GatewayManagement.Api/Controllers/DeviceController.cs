2fe4562ab0a1702875f8812a4224234ebab8dd4b
Mutaherul Islam <babu747d@gmail.com>
2021-09-23 01:39:56 +0600

    Add files via upload

---
+++ src/CompanyName.GatewayManagement.Api/Controllers/DeviceController.cs
@@ -0,0 +1,78 @@
+﻿using CompanyName.GatewayManagement.Api.Presenter;
+using CompanyName.GatewayManagement.Domain.DTO;
+using CompanyName.GatewayManagement.Domain.Exceptions;
+using CompanyName.GatewayManagement.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CompanyName.GatewayManagement.Api.Controllers
+{
+    [Route("gateways/{id}")]
+    [ApiController]
+    public class DeviceController : BaseController
+    {
+        private readonly IGatewayService _gatewayService;
+        private readonly IDeviceService _deviceService;
+        public DeviceController(IGatewayService gatewayService, IDeviceService deviceService, IResponseWrappable wrapper) : base(wrapper)
+        {
+            _gatewayService = gatewayService;
+            _deviceService = deviceService;
+        }
+        /// <summary>
+        /// Get all devices of a gateway
+        /// </summary>
+        [HttpGet]
+        [Route("devices")]
+        public async Task<ActionResult<object>> GetDevices(long id)
+        {
+            var response = await _gatewayService.GetGateway(id);
+            return Ok(await _wrapper.Get(response.PeripheralDevices));
+        }
+        /// <summary>
+        /// Add new device under a gateway
+        /// </summary>
+        [HttpPost]
+        [Route("devices")]
+        public async Task<ActionResult<object>> AddDevice(long id, [FromBody] DeviceRequestDto device)
+        {
+            var response = await _deviceService.AddDevice(id, device);
+            return Ok(await _wrapper.Get(response));
+        }
+
+        /// <summary>
+        /// Get a specific device according to it's UId
+        /// </summary>
+        [HttpGet]
+        [Route("devices/{deviceUid}")]
+        public async Task<ActionResult<object>> GetDevice(long id, long deviceUid)
+        {
+            await GetGateway(id);
+            var response = await _deviceService.GetDevice(deviceUid);
+            return Ok(await _wrapper.Get(response));
+        }
+
+
+        /// <summary>
+        /// Delete a specific device according to it's UId
+        /// </summary>
+        [HttpDelete]
+        [Route("devices/{deviceUid}")]
+        public async Task<ActionResult<object>> RemoveDevice(long id, long deviceUid)
+        {
+            await GetGateway(id);
+
+            var response = await _deviceService.RemoveDevice(deviceUid);
+            return Ok(await _wrapper.Get(response));
+        }
+
+        private async Task<GatewayDetailsDto> GetGateway(long id)
+        {
+            var gateway = await _gatewayService.GetGateway(id);
+            if (gateway == null)
+            {
+                throw new GatewayManagementNotFoundResultException("Gateway not found.", ErrorCode.E0404.ToString());
+            }
+            return gateway;
+        }
+    }
+}
---
+++ src/CompanyName.GatewayManagement.Api/Controllers/GatewayController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CompanyName.GatewayManagement.Api.Presenter;
+using CompanyName.GatewayManagement.Domain.DTO;
+using CompanyName.GatewayManagement.Domain.Interfaces;
+using System.Threading.Tasks;
+
+namespace CompanyName.GatewayManagement.Api.Controllers
+{
+    [Route("gateways")]
+    [ApiController]
+    public class GatewayController : BaseController
+    {
+        private readonly IGatewayService _gatewayService;
+
+        public GatewayController(IGatewayService gatewayService, IResponseWrappable wrapper) : base(wrapper)
+        {
+            _gatewayService = gatewayService;
+        }
+
+        /// <summary>
+        /// Get All Gateways with it's associated devices
+        /// </summary>
+        [HttpGet]
+        public async Task<ActionResult<object>> GetAllGateways()
+        {
+            var response = await _gatewayService.GetAllGateways();
+            return Ok(await _wrapper.Get(response));
+        }
+
+        /// <summary>
+        /// Get a specific Gateway according to it's id
+        /// </summary>
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<object>> GetAllGateways(long id)
+        {
+            var response = await _gatewayService.GetGateway(id);
+            return Ok(await _wrapper.Get(response));
+        }
+
+        /// <summary>
+        /// Add new Gateway
+        /// </summary>
+        [HttpPost]
+        public async Task<ActionResult<object>> AddGateway([FromBody] GatewayRequestDto gateway)
+        {
+            var response = await _gatewayService.AddGateway(gateway);
+            return Ok(await _wrapper.Get(response));
+        }
+    }
+}
