41bb35c7eba003d328b86b172d7b4a717c11964e
Mutaherul Islam <babu747d@gmail.com>
2021-09-23 17:11:34 +0600

    Add files via upload

---
+++ CompanyName.GatewayManagement.Domain/DTO/BaseUpdateResponseDto.cs
@@ -0,0 +1,23 @@
+﻿namespace CompanyName.GatewayManagement.Domain.DTO
+{
+    public class BaseResponseDto
+    {
+        public BaseResponseDto()
+        {
+        }
+
+        public BaseResponseDto(string message)
+        {
+            Message = message;
+        }
+
+        public BaseResponseDto(bool status, string message)
+        {
+            Status = status;
+            Message = message;
+        }
+
+        public bool Status { get; set; } = true;
+        public string Message { get; set; }
+    }
+}
--- CompanyName.GatewayManagement.Domain/DTO/DeviceDto.cs
+++ CompanyName.GatewayManagement.Domain/DTO/DeviceDto.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace CompanyName.GatewayManagement.Domain.DTO
+{
+    public class DeviceResponseDto
+    {
+        public long Uid { get; set; }
+        public string VendorName { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
