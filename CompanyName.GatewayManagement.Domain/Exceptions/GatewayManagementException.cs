b983c81986c0c35ed854af4667c09cb7d595c0c9
Mutaherul Islam <babu747d@gmail.com>
2021-09-23 17:10:04 +0600

    Add files via upload

---
+++ CompanyName.GatewayManagement.Domain/Exceptions/GatewayManagementException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CompanyName.GatewayManagement.Domain.Exceptions
+{
+    [Serializable]
+    public class GatewayManagementException : Exception
+    {
+        protected GatewayManagementException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public GatewayManagementException(string message) : base(message)
+        {
+        }
+
+        public GatewayManagementException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
