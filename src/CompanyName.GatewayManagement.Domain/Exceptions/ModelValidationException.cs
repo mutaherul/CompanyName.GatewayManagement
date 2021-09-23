b4db12976ff233d64c586649ac989ad8d92ca0c1
Mutaherul Islam <babu747d@gmail.com>
2021-09-23 22:43:16 +0600

    Add files via upload

---
+++ src/CompanyName.GatewayManagement.Domain/Exceptions/ModelValidationException.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Runtime.Serialization;
+
+namespace CompanyName.GatewayManagement.Domain.Exceptions
+{
+    [Serializable]
+    public class ModelValidationException : GatewayManagementException
+    {
+        public ValidationResult ValidationResult { get; private set; }
+
+        protected ModelValidationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public ModelValidationException(string message, ValidationResult result)
+            : base(message)
+        {
+            ValidationResult = result;
+        }
+    }
+}
