86745739b1aa804f9ec8dd48b3afd219ebd9b740
Mutaherul Islam <babu747d@gmail.com>
2021-09-23 22:41:36 +0600

    Add files via upload

--- src/CompanyName.GatewayManagement.Domain/Interfaces/IDeviceService.cs
+++ src/CompanyName.GatewayManagement.Domain/Interfaces/IDeviceService.cs
@@ -1,1 +1,12 @@
+﻿using CompanyName.GatewayManagement.Domain.DTO;
+using System.Threading.Tasks;
 
+namespace CompanyName.GatewayManagement.Domain.Interfaces
+{
+    public interface IDeviceService
+    {
+        Task<BaseResponseDto> AddDevice(long gatewayId, DeviceRequestDto device);
+        Task<DeviceResponseDto> GetDevice(long deviceUid);
+        Task<BaseResponseDto> RemoveDevice(long uid);
+    }
+}
---
+++ src/CompanyName.GatewayManagement.Domain/Interfaces/IGatewayService.cs
@@ -0,0 +1,13 @@
+﻿using CompanyName.GatewayManagement.Domain.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CompanyName.GatewayManagement.Domain.Interfaces
+{
+    public interface IGatewayService
+    {
+        Task<List<GatewayDetailsDto>> GetAllGateways();
+        Task<GatewayDetailsDto> GetGateway(long id);
+        Task<BaseResponseDto> AddGateway(GatewayRequestDto gateway);
+    }
+}
