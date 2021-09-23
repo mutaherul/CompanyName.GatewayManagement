503b3ce214d467b850550326caedb334461533c6
Mutaherul Islam <babu747d@gmail.com>
2021-09-23 18:17:53 +0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 echo "Restore dependencies and tools for the project "
 dotnet restore
 echo "Building the solution...."
-dotnet build MusalaSoft.GatewayManagement.sln
+dotnet build CompanyName.GatewayManagement.sln
 echo "Running unit tests...."
-dotnet test --no-restore .\tests\MusalaSoft.GatewayManagement.Domain.Tests\MusalaSoft.GatewayManagement.Domain.Tests.csproj
-dotnet publish MusalaSoft.GatewayManagement.sln
+dotnet test --no-restore .\tests\CompanyName.GatewayManagement.Domain.Tests\CompanyName.GatewayManagement.Domain.Tests
+dotnet publish CompanyName.GatewayManagement.sln
 PAUSE 
