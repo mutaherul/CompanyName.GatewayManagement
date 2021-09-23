012fc47f8b9d1d771c316a0abcba00b0912045b4
Mutaherul Islam <babu747d@gmail.com>
2021-09-23 23:53:29 +0600

    Add files via upload

---
+++ tests/CompanyName.GatewayManagement.Domain.Tests/Helper/AutoMapperConfigurationManager.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CompanyName.GatewayManagement.Domain.Tests.Helper
+{
+    public static class AutoMapperConfigurationManager
+    {
+        private static IMapper _mapper;
+
+        public static IMapper Mapper
+        {
+            get
+            {
+                if (_mapper == null)
+                {
+                    var mapperConfiguration = new MapperConfiguration(configExp =>
+                    {
+                        var assembly = Assembly.Load("CompanyName.GatewayManagement.Domain");
+                        var types = assembly.GetTypes();
+                        var profiles = types.Where(it => it.IsSubclassOf(typeof(Profile))).ToList();
+                        profiles.ForEach(obj => configExp.AddProfile((Profile)Activator.CreateInstance(obj)));
+                    });
+                    _mapper = mapperConfiguration.CreateMapper();
+                }
+                return _mapper;
+            }
+        }
+    }
+}
